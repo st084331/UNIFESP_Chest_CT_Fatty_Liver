425ad45942e667d42f26564d2c7c74192ce3b486
st084331 <71183199+st084331@users.noreply.github.com>
2022-12-17 14:21:15 +0300

    print added

--- interval_model.py
+++ interval_model.py
@@ -4,7 +4,6 @@ import math
 import sklearn
 from datetime import datetime
 
-
 places = ['whole_liver', 'one_area', 'two_areas', 'three_areas', 'random_points', 'random_point']
 score_dicts = []
 for place in places:
@@ -127,7 +126,7 @@ for place in places:
                 border_point = rightmost_point
             else:
                 border_point = leftmost_point
-            print(place, type ,border_point, i, datetime.now().strftime("%H:%M:%S"))
+            print(place, type, border_point, i, datetime.now().strftime("%H:%M:%S"))
 
             for j in range(1, 31):
                 test = []
